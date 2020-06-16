e04c0bab412a07d098a6e339ef81575ab1667893
Louis Juliendo <louisjuliendo@gmail.com>
2020-06-16 19:42:10 +0700

    Refactor hard-coded boards using 2 for-loops

--- src/index.js
+++ src/index.js
@@ -22,26 +22,28 @@ class Board extends React.Component {
     );
   }
 
+  createBoard() {
+    const board = [];
+    const boardSize = 3;
+    let index = 0;
+
+    for (let row = 0; row < boardSize; row++) {
+      let cols = [];
+      for (let col = 0; col < boardSize; col++) {
+        cols.push(this.renderSquare(index++));
+      }
+      board.push(
+        <div key={row} className="board-row">
+          {cols}
+        </div>
+      );
+    }
+
+    return board;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.createBoard()}</div>;
   }
 }
 
